da3ae00b0c4f046b9a27271746e9442ee7781da9
Personal <adriankenneally06@gmail.com>
2022-10-26 13:53:07 +0200

    adding gallery page view and side scrolling bar

---
+++ js/gallery.js
@@ -0,0 +1,8 @@
+// init Masonry after all images have loaded
+var $grid = $('.grid').imagesLoaded( function() {
+    $grid.masonry({
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      columnWidth: '.grid-sizer'
+    }); 
+  });
